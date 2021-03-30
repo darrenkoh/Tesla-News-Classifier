69e17c3ef59a5921a72db70c2da88e7c793d0f8b
darrenkoh <darrenkoh@gmail.com>
2021-03-30 15:54:06 -0700

    Restructure crawler code

---
+++ start.sh
@@ -0,0 +1,2 @@
+#docker run --name tesla_classifier --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v /Users/darrenkoh/dev/tesla_classifier:/home/jovyan/work darrenkoh/ml-jupyter
+docker run --name tesla_classifier -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v /Users/darrenkoh/project/Tesla-News-Classifier:/home/jovyan/work jupyter/scipy-notebook
