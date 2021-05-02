895be11d443c3f2622afda18ebcac7e79864f399
darrenkoh <darrenkoh@gmail.com>
2021-05-02 00:12:07 -0700

    Update training data

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #docker run --name tesla_classifier --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v /Users/darrenkoh/dev/tesla_classifier:/home/jovyan/work darrenkoh/ml-jupyter
 docker run --name tesla_classifier -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v /Users/darrenkoh/project/Tesla-News-Classifier:/home/jovyan/work jupyter/scipy-notebook
+#docker run --rm --name tesla_classifier -p 8888:8888 -v /Users/darrenkoh/project/Tesla-News-Classifier:/workspace huggingface/transformers-pytorch-cpu jupyter notebook --ip 0.0.0.0 --allow-root
